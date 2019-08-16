da4f607c7424770f9022b49fe7d60216b033a1dc
fqj <390933756@qq.com>
2019-08-17 00:22:59 +0800

    初始化

---
+++ src/greet.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export class Greet extends Component {
+
+    render() {
+        return (
+            <div>
+                11111
+            </div>
+        )
+    }
+}
+
+export default Greet
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import {render} from 'react-dom';
+import Gtwo from './Antv/index'
+// import Echarts from './Echarts/index'
+
+render(<Gtwo />, document.getElementById('root'));
