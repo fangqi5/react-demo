02d69a090895fcb5f4a54c6eeb01b148fba4ee8e
fangqiji <fangqiji@cnstrong.cn>
2019-10-17 17:00:08 +0800

    添加判断是否展示创建战队按钮接口&&回退误传的eslint配置

--- src/containers/learnreact/index.jsx
+++ src/containers/learnreact/index.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { Component,Fragment } from 'react'
 import ApplyHandle from 'COMPONENTS/modal'
 import styles from './index.scss'
 
@@ -8,6 +8,7 @@ export class LearnReact extends Component {
         this.state = {
             visiable:false
         }
+        this.ref = React.createRef();
     }
 
     close = () =>{
@@ -22,10 +23,14 @@ export class LearnReact extends Component {
         })
     }
 
+    input = (e) =>{
+        console.log(this.ref.current.value)
+    }
+
     render() {
         const { visiable } = this.state
-        const module = (
-            <div className={styles['container']}>
+        const modal = (
+            <Fragment>
                 <div onClick={this.show}>测试弹窗</div>
                 <ApplyHandle visiable={visiable}>
                     <div className={styles['wrap']}>
@@ -38,6 +43,14 @@ export class LearnReact extends Component {
                         </div>
                     </div>
                 </ApplyHandle>
+            </Fragment>
+        )
+
+
+        const module = (
+            <div className={styles['container']}>
+                <input placeholder="这是一个输入框" ref={this.ref} onChange={this.input} />
+                { modal }
             </div>
         )
         return module;
