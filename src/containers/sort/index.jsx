97be4377126bc256cca09a2de690ef8fb9a3a097
fangqiji <fangqi9697@163.com>
2019-11-15 09:56:02 +0800

    完善选择排序逻辑

--- src/containers/sort/index.jsx
+++ src/containers/sort/index.jsx
@@ -15,7 +15,7 @@ export class Sort extends Component {
         console.log(data)
         let c;
         for(let i=0;i<data.length - 1;i++){
-            for(let j=0;j<data.length - 1;j++){
+            for(let j=0;j<data.length - 1;j++){ //从第一个元素开始依次往后对比，在倒数第二个元素对比结束后，最后一个元素应该也是最大的，所以循环次数为数组长度 - 1
                 if(data[j] > data[j + 1 ] ){
                     c = data[j]
                     data[ j ] = data[ j+1 ]
@@ -33,7 +33,7 @@ export class Sort extends Component {
         let { data } = this.state
         console.log(data)
         let c;
-        for(let i=1;i<data.length;i++){
+        for(let i=1;i<data.length;i++){ //由于第一个默认是已排序元素，排序方式为从第二个元素开始依次向前比较，所以循环次数为数组长度 - 1
             for(let j=i;j>=0;j--){
                 if(data[j] < data[j-1]){
                     c = data[j] 
@@ -51,10 +51,10 @@ export class Sort extends Component {
     selectSort = () => {
         let { data } = this.state
         console.log(data)
-        let c = 0;
         let d;
         for(let i=0;i<data.length;i++){
-            for(let j=i;j<data.length - 1;j++){
+            let c = i;
+            for(let j=i;j<data.length;j++){ //关于内层循环的次数，选择排序是要从第一个开始到最后一个，所以循环次数要等于数组长度
                 if(data[j] < data[c]){
                     c = j
                 }
