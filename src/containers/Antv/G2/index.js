4a615b4346cb013511fdfd397fbdbe0af4ce0089
fangqiji <fangqi9697@163.com>
2019-12-11 17:47:42 +0800

    简单版轮播图实现

--- src/containers/Antv/G2/index.js
+++ src/containers/Antv/G2/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import G2 from '@antv/g2';
-import dataList from './data'
+// import dataList from './data'
 export class Gtwo extends Component {
 
     componentDidMount(){
@@ -8,6 +8,14 @@ export class Gtwo extends Component {
     }
 
     initBar(){
+        let data = [
+            { year:'撒的撒打算1',value:1.2,miss:false },
+            { year:'撒的撒打算2',value:0.2,miss:false },
+            { year:'撒的撒打算3',value:null,miss:true },
+            { year:'撒的撒打算4',value:1.5,miss:false },
+            { year:'撒的撒打算5',value:2.4,miss:false },
+            { year:'撒的撒打算6',value:3,miss:false },
+        ]
           // Step 1: 创建 Chart 对象
           const chart = new G2.Chart({
             container: 'c1', // 指定图表容器 ID
@@ -16,10 +24,49 @@ export class Gtwo extends Component {
             padding:'auto'
           });
           // Step 2: 载入数据源
-          chart.source(dataList.data);
+          chart.source(data,function(data){
+              console.log(data)
+          });
+          chart.axis('value',{
+            label:{
+                // formatter(text, item,index) {
+                //     console.log(text,item,index)
+                //     if(text < 0){
+                //         return text = 0
+                //     }
+                //     return text
+                // },
+            }
+          })
           chart.legend(false)
+        //   chart.tooltip({
+        //     useHtml:true, 
+        //     htmlContent:function htmlContent(title,items){
+        //        return `<div class="g2-tooltip">
+        //             <div class="custom-tooltip-title" style="margin-bottom: 0px;">${ title }</div>
+        //             <ul class="custom-tooltip-items">
+        //                 <li class="custom-tooltip-item">
+        //                 ${ console.log(title,items) }
+        //                 </li>
+        //                 <li class="custom-tooltip-item">
+        //                 ${ items }
+        //                 </li>
+        //             </ul>
+        //        </div>`
+        //     }
+        //     // itemTpl: '<li data-index={index}>' +
+        //     //     '<span style="background-color:{color};width:8px;height:8px;border-radius:50%;display:inline-block;margin-right:8px;"></span>' +
+        //     //     `{name}: ` +
+        //     //     '</li>'
+        //   })
           // Step 3：创建图形语法，绘制柱状图，由 genre 和 sold 两个属性决定图形位置，genre 映射至 x 轴，sold 映射至 y 轴
-          chart.line().position('year*value').color('value')
+          chart.line().position('year*value').color('value').tooltip('year*value',function(year,value){
+              return{
+                title: year,
+                name: '班平均分',
+                value: value === null ? '缺考' : value,
+              }
+          })
           chart.point().position('year*value').color('value').size('value', val => {
               return val&&3
           })
--- src/containers/carousel/index.jsx
+++ src/containers/carousel/index.jsx
@@ -1,15 +1,73 @@
 import React, { Component } from 'react'
+import styles from './index.scss'
+import pic1 from '../../assets/curousel/durant.jpg'
+import pic2 from '../../assets/curousel/Jame.jpg'
+import pic3 from '../../assets/curousel/Kobe.jpg'
 // import PropTypes from 'prop-types'
-
+let curPicIndex = 1
+// let duration = 5
+let translateX = 0
 export class Carousel extends Component {
-    static propTypes = {
-
+    constructor(props){
+        super(props);
+        this.state = {
+            imgs:[
+                pic1,pic2,pic3,pic1
+            ],
+            curPicIndex : 1
+        }
     }
 
-    render() {
+    componentDidMount(){
+        this.action()
+        // window.addEventListener('')
+    }
+
+    action = () => {
+        let li = document.querySelectorAll('li')
+        if(curPicIndex <= 3){
+            if(translateX != 200*curPicIndex){
+                li.forEach( item =>{
+                    item.style.transform = `translateX(${ -translateX + 'px' })`
+                } )
+                translateX = translateX + 1
+                requestAnimationFrame(this.action)
+            }else{
+                
+                setTimeout(() => {
+                    if(curPicIndex ===3 && translateX === 200 * curPicIndex){
+                        translateX = 0
+                        li.forEach( item =>{
+                            item.style.transform = `translateX(${ -translateX + 'px' })`
+                        } )
+                        curPicIndex = 1
+                        translateX = translateX + 1
+                        requestAnimationFrame(this.action)
+                    }else{
+                        translateX = translateX + 1
+                        requestAnimationFrame(this.action)
+                    }
+                }, 1000);
+            }
+            translateX <= 200*curPicIndex ? '' : curPicIndex = curPicIndex + 1
+        }
+    }
+
+    render(){
+        const { imgs } = this.state
         return (
-            <div>
-                11111
+            <div className={styles['curousel']}>
+                <ul className={styles['view']}>
+                {
+                    imgs.map((item,index)=>{
+                        return (
+                            <li key={index}>
+                                <img src={ item } />
+                            </li>
+                        )
+                    })
+                }
+                </ul>
             </div>
         )
     }
