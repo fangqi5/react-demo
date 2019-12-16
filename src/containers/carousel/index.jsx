fca75d369a54f06bcefbd5146a65232a84556f0a
fangqiji <fangqi9697@163.com>
2019-12-16 17:48:04 +0800

    同步代码

--- src/containers/carousel/index.jsx
+++ src/containers/carousel/index.jsx
@@ -1,87 +1,21 @@
 import React, { Component } from 'react'
 import styles from './index.scss'
-import pic1 from '../../assets/curousel/durant.jpg'
-import pic2 from '../../assets/curousel/Jame.jpg'
-import pic3 from '../../assets/curousel/Kobe.jpg'
+import First from './first'
+import Second from './second'
 // import PropTypes from 'prop-types'
-let curPicIndex = 1
-// let duration = 5
-let translateX = 0
-export class Carousel extends Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            imgs:[
-                pic1,pic2,pic3,pic1
-            ],
-            num:[1,2,3],
-            curPicIndex : 1
-        }
-    }
 
-    componentDidMount(){
-        this.action()
-    }
+export class index extends Component {
 
-    action = () => {
-        let li = document.querySelectorAll('li')
-        let point = document.querySelectorAll('.points')
-        if(translateX !== 200*curPicIndex){
-            li.forEach( item =>{
-                item.style.transform = `translateX(${ -translateX + 'px' })`
-            } )
-            translateX = translateX + 1
-            requestAnimationFrame(this.action)
-        }else{
-            curPicIndex = curPicIndex + 1
-            setTimeout(() => {
-                if(curPicIndex > 3 ){
-                    curPicIndex = 1 
-                    translateX = 0
-                    requestAnimationFrame(this.action)
-                }else{
-                    translateX = translateX + 1
-                    requestAnimationFrame(this.action)
-                }
-            }, 1000);
-        }
-        point.forEach( (dom,cur) =>{
-            if(cur + 1 === curPicIndex){
-                dom.style.backgroundColor = 'red'
-            }else{
-                dom.style.backgroundColor = 'blue'
-            }
-        })
-
-        if(curPicIndex === 4){
-            point[0].style.backgroundColor = 'red'
-        }
-            
-    }
-
-    render(){
-        const { imgs,num } = this.state
+    render() {
         return (
-            <div className={styles['curousel']}>
-                <ul className={styles['view']}>
-                {
-                    imgs.map((item,index)=>{
-                        return (
-                            <li key={index}>
-                                <img src={ item } />
-                            </li>
-                        )
-                    })
-                }
-                { num.map((blank,num)=>{
-                    return (
-                        <span key={num} style={{ left:80 + num * 20 + 'px'}} className={`points ${styles['point']}`}></span>
-                    )
-                }) }
-                </ul>
+            <div className={styles['carouselContainer']}>
+                <span>第一阶段实现</span>
+                <First />
+                <span>第二阶段实现</span>
+                <Second />
             </div>
         )
     }
 }
 
-export default Carousel
+export default index
---
+++ src/containers/carousel/second/index.jsx
@@ -0,0 +1,88 @@
+import React, { Component } from 'react'
+import styles from './index.scss'
+import pic1 from '../../../assets/curousel/durant.jpg'
+import pic2 from '../../../assets/curousel/Jame.jpg'
+import pic3 from '../../../assets/curousel/Kobe.jpg'
+// import PropTypes from 'prop-types'
+let curPicIndex = 1
+let translateX = 0
+export class Carousel extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            imgs:[
+                pic1,pic2,pic3,pic1
+            ],
+            num:[1,2,3],
+            curPicIndex : 1
+        }
+    }
+
+    componentDidMount(){
+        this.action()
+    }
+
+    action = () => {
+        let li = document.querySelectorAll('li')
+        let point = document.querySelectorAll('.secPoints')
+        if(translateX !== 200*curPicIndex){
+            li.forEach( item =>{
+                item.style.transform = `translateX(${ -translateX + 'px' })`
+            } )
+            translateX = translateX + 1
+            requestAnimationFrame(this.action)
+        }else{
+            curPicIndex = curPicIndex + 1
+            setTimeout(() => {
+                if(curPicIndex > 3 ){
+                    curPicIndex = 1 
+                    translateX = 0
+                    requestAnimationFrame(this.action)
+                }else{
+                    translateX = translateX + 1
+                    requestAnimationFrame(this.action)
+                }
+            }, 1000);
+        }
+        point.forEach( (dom,cur) =>{
+            if(cur + 1 === curPicIndex){
+                dom.style.backgroundColor = 'red'
+            }else{
+                dom.style.backgroundColor = 'blue'
+            }
+        })
+
+        if(curPicIndex === 4){
+            point[0].style.backgroundColor = 'red'
+        }
+            
+    }
+
+    render(){
+        const { imgs,num } = this.state
+        return (
+            <div className={styles['curousel']}>
+                <ul className={styles['view']}>
+                {
+                    imgs.map((item,index)=>{
+                        return (
+                            <li key={index}>
+                                <img src={ item } />
+                            </li>
+                        )
+                    })
+                }
+                { 
+                    num.map((blank,num)=>{
+                        return (
+                            <span key={num} style={{ left:80 + num * 20 + 'px'}} className={`secPoints ${styles['point']}`}></span>
+                        )
+                    }) 
+                }
+                </ul>
+            </div>
+        )
+    }
+}
+
+export default Carousel
